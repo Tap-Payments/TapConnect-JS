4d9e2cf89f1c8fa34a44269acf03648df7ee565e
kholdunn <k.naqawa@tap.company>
2020-09-08 04:22:57 -0400

    centralize

--- demo/src/App.js
+++ demo/src/App.js
@@ -3,66 +3,76 @@ import { TapAuthButton, DialogMode, AnimationType, PageMode } from '../../src/in
 function App(props) {
   return (
     <Fragment>
-      <div style={{ width: '230px', marginBottom: '15px' }}>
-        <TapAuthButton
-          // initialLeadID={vm.initialLeadID}
-          pageMode={PageMode.CONNECT}
-          buttonText={'connect'}
-          countryCode={'965'}
-          animationType={AnimationType.SLIDEUP}
-          animationDuration={500}
-          closeOnOutsideClick={false}
-          // defaultEmailOrMobile={vm.initialUsername}
-          hideInitialLoader={props.hideInitialLoader}
-          // onSuccess={vm.onSuccess}
-          theme={
-            {
-              // direction: vm.appDirection,
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <div style={{ width: '230px', marginBottom: '15px' }}>
+          <TapAuthButton
+            // initialLeadID={vm.initialLeadID}
+            pageMode={PageMode.CONNECT}
+            buttonText={'connect'}
+            countryCode={'965'}
+            animationType={AnimationType.SLIDEUP}
+            animationDuration={500}
+            closeOnOutsideClick={false}
+            // defaultEmailOrMobile={vm.initialUsername}
+            hideInitialLoader={props.hideInitialLoader}
+            // onSuccess={vm.onSuccess}
+            theme={
+              {
+                // direction: vm.appDirection,
+              }
             }
-          }
-        />
-      </div>
-      <div style={{ width: '230px', marginBottom: '15px' }}>
-        <TapAuthButton
-          // initialLeadID={vm.initialLeadID}
-          pageMode={PageMode.LOGIN}
-          buttonText={'login'}
-          countryCode={'965'}
-          // onCancel={vm.onCancel}
-          animationType={AnimationType.SLIDEUP}
-          animationDuration={500}
-          closeOnOutsideClick={false}
-          // defaultEmailOrMobile={vm.initialUsername}
-          hideInitialLoader={props.hideInitialLoader}
-          // moveToSignup={vm.moveToSignup}
-          // onSuccess={vm.onSuccess}
-          theme={
-            {
-              // direction: vm.appDirection,
+          />
+        </div>
+        <div style={{ width: '230px', marginBottom: '15px' }}>
+          <TapAuthButton
+            // initialLeadID={vm.initialLeadID}
+            pageMode={PageMode.LOGIN}
+            buttonText={'login'}
+            countryCode={'965'}
+            // onCancel={vm.onCancel}
+            animationType={AnimationType.SLIDEUP}
+            animationDuration={500}
+            closeOnOutsideClick={false}
+            // defaultEmailOrMobile={vm.initialUsername}
+            hideInitialLoader={props.hideInitialLoader}
+            // moveToSignup={vm.moveToSignup}
+            // onSuccess={vm.onSuccess}
+            theme={
+              {
+                // direction: vm.appDirection,
+              }
             }
-          }
-        />
-      </div>
-      <div style={{ width: '230px' }}>
-        <TapAuthButton
-          // initialLeadID={vm.initialLeadID}
-          pageMode={PageMode.SIGNUP}
-          buttonText={'signup'}
-          countryCode={'965'}
-          // onCancel={vm.onCancel}
-          animationType={AnimationType.SLIDEUP}
-          animationDuration={500}
-          closeOnOutsideClick={false}
-          // defaultEmailOrMobile={vm.initialUsername}
-          hideInitialLoader={props.hideInitialLoader}
-          // moveToLogin={vm.moveToLogin}
-          // onSuccess={vm.onSuccess}
-          theme={
-            {
-              // direction: vm.appDirection,
+          />
+        </div>
+        <div style={{ width: '230px' }}>
+          <TapAuthButton
+            // initialLeadID={vm.initialLeadID}
+            pageMode={PageMode.SIGNUP}
+            buttonText={'signup'}
+            countryCode={'965'}
+            // onCancel={vm.onCancel}
+            animationType={AnimationType.SLIDEUP}
+            animationDuration={500}
+            closeOnOutsideClick={false}
+            // defaultEmailOrMobile={vm.initialUsername}
+            hideInitialLoader={props.hideInitialLoader}
+            // moveToLogin={vm.moveToLogin}
+            // onSuccess={vm.onSuccess}
+            theme={
+              {
+                // direction: vm.appDirection,
+              }
             }
-          }
-        />
+          />
+        </div>
       </div>
     </Fragment>
   );
