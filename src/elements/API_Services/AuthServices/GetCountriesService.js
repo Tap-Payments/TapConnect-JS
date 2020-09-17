3a6e550bd6791c03b6c567fbb8751ada26735448
kholdunn <k.naqawa@tap.company>
2020-09-17 09:21:31 -0400

    remove country json

--- src/elements/API_Services/AuthServices/GetCountriesService.js
+++ src/elements/API_Services/AuthServices/GetCountriesService.js
@@ -1,32 +1,8 @@
 import constants from './constants';
 import axios from 'axios';
 import constant_business_country_temp from './const_business_country_data.json';
-import constant_country_temp from './constant_country_temp.json';
 const URL = 'https://utilities.tap.company/api/v1/country/list';
 class GetCountriesService {
-  static async getStaticCountriesData(callback) {
-    callback(constant_country_temp);
-    return;
-
-    var res;
-
-    await axios
-      .post(URL)
-      .then(async function (response) {
-        console.log(response);
-
-        res = response.data;
-        if (res.code == 200) {
-        }
-      })
-      .catch(function (error) {
-        // console.log(error);
-        return { error: 'error' };
-      });
-    // console.log(res);
-
-    await callback(res);
-  }
   static async getCountriesData(callback) {
     //// call API, bring raw data
 
