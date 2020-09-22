dae87ce1065de369f4b1251823c7f16884f9707a
kholdunn <k.naqawa@tap.company>
2020-09-22 05:44:40 -0400

    enable live

--- src/elements/API_Services/index.js
+++ src/elements/API_Services/index.js
@@ -1,3 +1,3 @@
 const SANDBOX_MW_URL = 'https://js-connect.dev.tap.company:3000/middleware';
-const LIVE_MW_URL = 'https://js-connect.dev.tap.company:3000/middleware'; //'https://js.connect.tap.company:3000/middleware';
+const LIVE_MW_URL = 'https://js.connect.tap.company:3000/middleware';
 export { SANDBOX_MW_URL, LIVE_MW_URL };
