7ad42739295cefa1251844221a318ec6898830c0
kholdunn <k.naqawa@tap.company>
2021-04-20 13:00:27 -0400

    filter country name

--- src/elements/Utils/filtering.js
+++ src/elements/Utils/filtering.js
@@ -11,7 +11,10 @@ export async function filterCountries(countryList) {
       country.digits == '' ||
       !country.idd_prefix ||
       country.idd_prefix == '' ||
-      country.idd_prefix == 0
+      country.idd_prefix == 0 ||
+      !country.name ||
+      !country.name.english ||
+      country.name.english.indexOf('?') != -1
       ? false
       : true;
   });
