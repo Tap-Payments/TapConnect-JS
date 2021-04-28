2999cdc54c2d9be73ed5c1fd329dad79a0c2d1eb
kholdunn <k.naqawa@tap.company>
2021-04-28 13:32:03 -0400

    update server url

--- src/elements/API_Services/index.js
+++ src/elements/API_Services/index.js
@@ -1,3 +1,3 @@
-const SANDBOX_MW_URL = 'https://js-connect.dev.tap.company/middleware';
+const SANDBOX_MW_URL = 'https://js-connect.sandbox.tap.company/middleware';
 const LIVE_MW_URL = 'https://js.connect.tap.company/middleware';
 export { SANDBOX_MW_URL, LIVE_MW_URL };
